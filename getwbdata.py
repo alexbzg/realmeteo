97c616725a247eb4f369addd333461cb001ad678
root <alexbzg@gmail.com>
2019-02-04 17:51:26 +0300

    weatherbit data converted to rmeteo format

--- getwbdata.py
+++ getwbdata.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python
 #coding=utf-8
 import urllib2
-from os import listdir, path
+from os import listdir
 import json
 import logging
 import argparse
 
-from rmconfig import rmconfig, appPath
+from rmconfig import rmconfig
 from rmutils import loadJSON
+
+def convert_forecast(data):
+    result = {}
+    result['epoch'] = data['ts']
+    result['icon'] = str(data['weather']['code'])
+    if data.has_key('temp'):
+        result['temperature'] = int(data['temp'])
+    if data.has_key('max_temp'):
+        result['temp_high'] = int(data['max_temp'])
+    if data.has_key('min_temp'):
+        result['temp_low'] = int(data['min_temp'])
+    result['pop'] = data['pop']
+    result['wind_dir'] = data['wind_cdir']
+    result['pressure'] = round(data['pres'] * 0.75, 1)
+    result['wind_speed'] = int(data['wind_spd'])
+    return result
 
 conf = rmconfig()
 data_dir = conf.get('site', 'path') + '/.weatherbit'
 api_url = conf.get('weatherbit', 'api_url')
 api_key = conf.get('weatherbit', 'key')
+
+forecast_length = {'daily': 10, 'hourly': 24}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', action="store_true")
@@ -47,10 +65,14 @@ for entry in listdir(data_dir):
         data = {'forecast': {}}
     if args.c:
         data['current'] = query(('current',))
-    if args.d:
-        data['forecast']['daily'] = query(('forecast', 'daily'))
-    if args.o:
-        data['forecast']['hourly'] = query(('forecast', 'hourly'))
+        data['current']['icon'] = str(data['current']['weather']['code'])
+    if args.d or args.o:
+        forecast_type = 'daily' if args.d else 'hourly'
+        forecast = query(('forecast', forecast_type))
+        data['forecast'][forecast_type] = []
+        for period in forecast:
+            data['forecast'][forecast_type].append(convert_forecast(period))
+        data['forecast'][forecast_type] = data['forecast'][forecast_type][:forecast_length[forecast_type]]
 
     with open(data_filename, 'w') as json_file:
         json_file.write(json.dumps(data))
